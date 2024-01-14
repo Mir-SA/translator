853e33639f0557ccfef2d5ad60f3ed19bf227940
Mir <65391345+Mir-SA@users.noreply.github.com>
2024-01-14 15:50:57 +0530

    translate route

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,104 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+
+function reverseObject(obj) {
+  let reversedObj = {};
+  for (let key in obj) {
+    reversedObj[obj[key]] = key;
+  }
+  return reversedObj;
+}
+let britishToAmericanSpelling = reverseObject(americanToBritishSpelling);
+let britishToAmericanTitles = reverseObject(americanToBritishTitles);
 
 class Translator {
+  validateLocale(locale) {
+    if (locale === "american-to-british" || locale === "british-to-american") {
+      return true;
+    }
+    return false;
+  }
 
+  translate(locale, text) {
+    if (locale === "american-to-british") {
+      return this.americanToBritish(text);
+    } else {
+      return this.britishToAmerican(text);
+    }
+  }
+
+  highlight(word) {
+    return `<span class=\"highlight\">${word}</span>`;
+  }
+
+  checkTime(locale, time) {
+    let regex = /\b\d{1,2}[:.]\d{2}\b/;
+    if (regex.test(time)) {
+      time =
+        locale === "american-to-british"
+          ? time.replace(":", ".")
+          : time.replace(".", ":");
+
+      return this.highlight(time);
+    }
+    return false;
+  }
+
+  americanToBritish(text) {
+    let txtArr = text.split(" ");
+    let translatedWords = 0;
+
+    let translated = txtArr.map((word) => {
+      word = americanToBritishTitles[word.toLowerCase()]
+        ? this.highlight(word.slice(0, word.length - 1))
+        : word;
+      word = americanOnly[word.toLowerCase()]
+        ? this.highlight(americanOnly[word.toLowerCase()])
+        : word;
+      word = americanToBritishSpelling[word.toLowerCase()]
+        ? this.highlight(americanToBritishSpelling[word.toLowerCase()])
+        : word;
+      word = this.checkTime("american-to-british", word)
+        ? this.checkTime("american-to-british", word)
+        : word;
+
+      if (word.includes("</span>")) translatedWords++;
+      return word;
+    });
+
+    return translatedWords === 0
+      ? "Everything looks good to me!"
+      : translated.join(" ");
+  }
+
+  britishToAmerican(text) {
+    let txtArr = text.split(" ");
+    let translatedWords = 0;
+
+    let translated = txtArr.map((word) => {
+      word = britishToAmericanTitles[word.toLowerCase()]
+        ? this.highlight(word + ".")
+        : word;
+      word = britishOnly[word.toLowerCase()]
+        ? this.highlight(britishOnly[word.toLowerCase()])
+        : word;
+      word = britishToAmericanSpelling[word.toLowerCase()]
+        ? this.highlight(britishToAmericanSpelling[word.toLowerCase()])
+        : word;
+      word = this.checkTime("british-to-american", word)
+        ? this.checkTime("british-to-american", word)
+        : word;
+
+      if (word.includes("</span>")) translatedWords++;
+      return word;
+    });
+
+    return translatedWords === 0
+      ? "Everything looks good to me!"
+      : translated.join(" ");
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,24 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    let { text } = req.body;
+    let { locale } = req.body;
+
+    if (text == undefined || locale == undefined) {
+      res.send({ error: "Required field(s) missing" });
+    } else if (text === "") {
+      res.send({ error: "No text to translate" });
+    } else if (!translator.validateLocale(locale)) {
+      res.send({ error: "Invalid value for locale field" });
+    } else {
+      let translation = translator.translate(locale, text);
+      console.log("Translated: ", translation);
+      res.send({ text, translation });
+    }
+  });
 };
